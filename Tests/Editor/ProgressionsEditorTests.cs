6f7cd78aa6004fcb37dc3fcad8c5850a9ab7fd8a
Cory Leach <cory.leach@gmail.com>
2019-11-20 18:27:17 -0500

    Fix warnings.

---
+++ Tests/Editor/ProgressionsEditorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Gameframe.Progressions.Tests.Editor
+{
+    public class ProgressionsEditorTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ProgressionsEditorTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ProgressionsEditorTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
