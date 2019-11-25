6b739533d76e2657a415513a45277ab09c050c3b
Cory Leach <cory.leach@gmail.com>
2019-11-24 20:13:22 -0500

    Float progression model formula

--- Editor/ProgressionModelEditor.cs
+++ Editor/ProgressionModelEditor.cs
@@ -4,40 +4,83 @@ using UnityEditor;
 
 namespace Gameframe.Progressions
 {
-  [CustomEditor(typeof(FloatProgressionModel),true)]
+  [CustomEditor(typeof(CalculatedFloatProgressionModel),true)]
   public class ProgressionModelEditor : UnityEditor.Editor
   {
-
-    int min = 0;
-    int max = 99;
-
-    SerializedProperty curve;
-
+    private SerializedProperty curve;
+    private SerializedProperty segments;
+    private SerializedProperty maxLevel;
+    
     private void OnEnable()
     {
       curve = serializedObject.FindProperty("curve");
+      segments = serializedObject.FindProperty("segments");
+      maxLevel = serializedObject.FindProperty("maxLevel");
     }
 
     public override void OnInspectorGUI()
     {
-      base.OnInspectorGUI();
+      //base.OnInspectorGUI();
 
       serializedObject.Update();
 
+      EditorGUILayout.PropertyField(maxLevel);
+      
+      int segmentCount = segments.arraySize;
+      if (segmentCount < 1)
+      {
+        segmentCount = 1;
+      }
+
+      EditorGUI.BeginChangeCheck();
+      segmentCount = EditorGUILayout.IntField("Segments", segmentCount);
+      if (EditorGUI.EndChangeCheck())
+      {
+        segments.arraySize = segmentCount;
+      }
+      
+      for (int i = 0; i < segments.arraySize; i++)
+      {
+        EditorGUILayout.BeginHorizontal();
+
+        var segment = segments.GetArrayElementAtIndex(i);
+
+        var exponent = segment.FindPropertyRelative("exponent");
+        var multiplierA = segment.FindPropertyRelative("multiplierA");
+        var multiplierB = segment.FindPropertyRelative("multiplierB");
+        var constant = segment.FindPropertyRelative("constant");
+        var minLevel = segment.FindPropertyRelative("minLevel");
+
+        minLevel.intValue = Mathf.RoundToInt((maxLevel.intValue / (float)segmentCount)  * i);
+        
+        multiplierA.floatValue = EditorGUILayout.FloatField(multiplierA.floatValue);
+        GUILayout.Label(" * X^");
+        exponent.floatValue = EditorGUILayout.FloatField(exponent.floatValue);
+        GUILayout.Label(" + ");
+        multiplierB.floatValue = EditorGUILayout.FloatField(multiplierB.floatValue);
+        GUILayout.Label(" * X + ");
+        constant.floatValue = EditorGUILayout.FloatField(constant.floatValue);
+        
+        GUILayout.Label($" if X >= {minLevel.intValue}");
+        
+        EditorGUILayout.EndHorizontal();
+      }
+      
       if ( curve != null )
       {
         curve.animationCurveValue = EditorGUILayout.CurveField(curve.animationCurveValue, GUILayout.Height(200));
       }
 
-      min = EditorGUILayout.IntField(min);
-      max = EditorGUILayout.IntField(max);
+      EditorGUILayout.BeginVertical("Box");
 
       var progressionModel = target as FloatProgressionModel;
-      for ( int i = min; i < max; i++ )
+      for ( int i = 0; i < maxLevel.intValue; i++ )
       {
         EditorGUILayout.LabelField(string.Format("{0}:{1}",i,progressionModel.Get(i)));
       }
 
+      EditorGUILayout.EndVertical();
+      
       serializedObject.ApplyModifiedProperties();
     }
 
--- Runtime/CalculatedFloatProgressionModel.cs
+++ Runtime/CalculatedFloatProgressionModel.cs
@@ -1,28 +1,32 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace Gameframe.Progressions
 {
+  
+  [System.Serializable]
+  public class ProgressionSegment
+  {
+    //Formula is : A * x^2 + B * X + C
+    public float exponent = 2;
+    public float multiplierA = 1;
+    public float multiplierB = 1f;
+    public float constant = 0;
+    public int minLevel = 0;    
+    
+    public float Get(int level)
+    {
+      level -= minLevel;
+      return multiplierA * Mathf.Pow(level, exponent) + multiplierB*level + constant;
+    }
+  }
+
   [CreateAssetMenu(menuName ="Gameframe/Progressions/Calculated/Float")]
   public class CalculatedFloatProgressionModel : FloatProgressionModel
   {
-    [SerializeField]
-    private float startValue = 0;
-
-    [System.Serializable]
-    public class ProgressionSegment
-    {
-      [SerializeField]
-      private float exponent = 1;
-
-      [SerializeField]
-      private float multiplier = 1;
-
-      public float Get(int level)
-      {
-        return multiplier * Mathf.Pow(level, exponent);
-      }
-    }
-
+    [SerializeField] 
+    private int maxLevel = 100;
+    
     [SerializeField]
     private ProgressionSegment[] segments = new ProgressionSegment[0];
 
@@ -33,27 +37,56 @@ namespace Gameframe.Progressions
 
     public override float Get(int level)
     {
-      if ( segments == null )
+      if ( segments == null || segments.Length == 0)
       {
         return 0;
       }
+      return segments[GetSegmentIndex(level)].Get(level);
+    }
 
-      float sum = 0;
-      for ( int i = 0; i < segments.Length; i++ )
+    private int GetSegmentIndex(int level)
+    {
+      int index = 0;
+      for (; index+1 < segments.Length; index++)
       {
-        sum += segments[i].Get(level);
+        var nextIndex = index + 1;
+        if (level < segments[nextIndex].minLevel )
+        {
+          return index;
+        }
       }
-      return Mathf.Round(startValue + sum);
-   }
+      return index;
+    }
 
+    #if UNITY_EDITOR
     private void OnValidate()
     {
+      for (int i = 1; i < segments.Length; i++)
+      {
+        //Make sure each segment increases the min level
+        if (segments[i].minLevel <= segments[i-1].minLevel)
+        {
+          segments[i].minLevel = segments[i].minLevel + 1;
+        }
+        //Constant of each segment should be the final value of the previous segment
+        segments[i].constant = segments[i - 1].Get(segments[i].minLevel);
+      }
+      
       curve = new AnimationCurve();
-      for ( int i = 0; i < 100; i++ )
+      
+      for ( int i = 0; i < maxLevel; i++ )
       {
         curve.AddKey(i, Get(i));
       }
-    }
 
+      for ( int i = 0; i < maxLevel; i++ )
+      {
+        AnimationUtility.SetKeyLeftTangentMode(curve, i, AnimationUtility.TangentMode.Linear);
+        AnimationUtility.SetKeyRightTangentMode(curve, i, AnimationUtility.TangentMode.Linear);
+      }
+      
+    }
+    #endif
+    
   }
 }
